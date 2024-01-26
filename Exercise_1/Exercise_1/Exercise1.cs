6b9a8aa62dfabc30617db75298ee1822c2c6186d
Kloee Pratt-Castaneda <148795872+kloeencastaneda@users.noreply.github.com>
2024-01-25 18:05:30 -0600

    Files added via upload

---
+++ Exercise_1/Exercise_1/Exercise1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input an integer to reverse and add:");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int RN = 0, SN = 0; //RN stands for reversed number and SN for sum of number
+
+            while (N > 0)
+            {
+
+                int digit = N % 10;  //Retrieve remainder of N
+                RN = RN * 10 + digit; //Multiply remainder by 10
+                SN += digit; //
+                N /= 10;
+
+            }
+
+            Console.WriteLine("Reversed integer: {0}", RN);
+            Console.WriteLine("Sum of integer: {0}", SN);
+
+            /////
+           
+        }
+    }
+}
