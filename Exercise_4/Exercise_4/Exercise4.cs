57707d94279ddf10c88f51fa2c59979321e3e686
Kloee Pratt-Castaneda <148795872+kloeencastaneda@users.noreply.github.com>
2024-01-25 18:11:42 -0600

    Exercise4

---
+++ Exercise_4/Exercise_4/Exercise4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exercise_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> LC = new Dictionary<char, int>(); //LC stands for letter count
+            char letter;
+
+            Console.WriteLine("Enter letters. To stop, press 0: ");
+
+            do
+            {
+                letter = Convert.ToChar(Console.ReadLine());
+                if (letter != '0')
+                {
+                    LC.TryGetValue(letter, out int count); //gets count value associated with letter
+                    LC[letter] = count + 1;
+                }
+            } while (letter != '0');
+
+            Console.WriteLine("Letter count: ");
+            foreach (KeyValuePair<char, int> pair in LC)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+
+
+            }
+        }
+    }
+}
